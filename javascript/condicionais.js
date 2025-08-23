63031b2c0d4a0980a5181ee0b0cc1dc1ceb0fb25
AdisonJ555 <kaneki.adison@gmail.com>
2025-08-22 21:50:00 -0300

    javaScript

---
+++ javascript/condicionais.js
@@ -0,0 +1,12 @@
+//exercicio 1 
+let idade = prompt("Qual e a idade");
+
+if (idade >= 18 ) {
+    alert("Parabéns! Voce pode ser Preso"); 
+
+} else 
+    alert("VAU CHAMAR SUA MÃE!!")
+
+    //exercicio 2
+
+    
---
+++ javascript/script.js
@@ -0,0 +1,26 @@
+console.log("Hello World!");
+
+// let-var-const= variaveis
+// let valorfixo = "Adison" 
+// let nome = prompt("Qual seu nome?");
+// let idade = prompt("Qual sua idade");
+
+// alert("Meu nome e " + nome + ", tenho tantos " + idade);
+
+let idade = prompt("Qual e a idade");
+
+if (idade >= 18 ) {
+    alert("Parabéns! Voce pode ser Preso"); 
+
+} else 
+    alert("VAU CHAMAR SUA MÃE!!")
+
+    let idade = prompt("Qual sua idade");
+
+
+
+
+
+
+
+
