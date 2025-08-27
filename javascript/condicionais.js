d282dd7613be044d6e7da79afb696a5207459949
AdisonJ555 <kaneki.adison@gmail.com>
2025-08-26 21:56:00 -0300

    nivel avancado

--- javascript/condicionais.js
+++ javascript/condicionais.js
@@ -5,116 +5,194 @@
 //     if (idade >= 60 ) { 
 
 //         alert("Idoso"); }
-       
+
 //     alert("Parabéns! Voce pode ser Preso"); 
 
 // } else 
 //     alert("VAU CHAMAR SUA MÃE!!");
 
-    //exercicio 2
+//exercicio 2
 
-    // let nota = prompt(nota);
+// let nota = prompt(nota);
 
-    // if (nota >= 60){ 
-     
-    //     alert("Aprovado");
+// if (nota >= 60){ 
 
-    // } else 
-    //     alert("Reprovado");
- 
+//     alert("Aprovado");
+
+// } else 
+//     alert("Reprovado");
+
 //     //exercicio 3
 
-    // let numero = prompt("Qual e o numero");
+// let numero = prompt("Qual e o numero");
+
+// if (numero > 0){
+
+
+//     alert("Positivo");  
+
+// } else {
+
+
+// if (numero == 0){
+
+//     alert ("O numero e zero");}
+
+//     else{
+//         alert("negativo")
+//     }
+// }
+
+//exercicio 5
+
+// let idade = prompt("Qual sua idade?")
+
+// if (idade <= 12) {
+
+//     alert("Kids")
+
+// }
+
+// else { 
+//          if (idade >= 18) {
+
+//     alert("Adulto") }
+
+// else{
+
+//     alert("Adolecente");}
+// }
+
+//exercicio 6
+
+// let numero = prompt("Insira um numero");
+
+// if (numero % 2 == 0  ) {
+
+//     alert("Par")
+
+// } else 
+
+//     alert("Impar")
+
+
+//   INTERMEDIARIO
+
+//   Exercicio 1
+
+// let numero = Number(prompt("Escolha um numero"));
+
+// let numero1 = Number(prompt("Escolha outro numero"));
+
+// let operacao = prompt("Insira a operação: +, -, *,/");
+
+// let rusultado = 0;
+
+// if (operacao == "+") {
+
+//     let resultado = numero + numero1;
+
+// } else {
+
+//     if (operacao == "-") {
+
+//         let resultado = numero - numero1;      
+
+//     } else {
+
+//         if (operacao == "*") {
+
+//             let resultado = numero * numero1;
+           
+//         } else {
+
+//             if (operacao == "/") {
+
+//                 let resultado = numero / numero1;
+
+//                
+//             }
+
+//         }
+//     }
+
+// }
+// alert(resultado);
+
+//                    exercicio I-2
+
+// let num1 = Number(prompt("Insira um numero"))
+// let num2 = Number(prompt("Mais um numero"))
+// let num3 = Number(prompt("Mais um"))
+
+// if (num1 > num2 && num1 > num3) {
+
+//     alert(num1);
+// } else {
+//     if (num2 > num1 && num2 > num3) {
+
+//         alert(num2)
+//     } else {
+//         alert(num3)
+//     }
+
+
+// }
+
+//                exercicio I-3
+
+// let compra = Number(prompt("Quanto deu sua compra"));
+
+// if (compra >= 100) {
+
+//     let descontos = compra * 0.1;
+
+//     compra = compra - descontos;
+
+//     alert(compra);
+// } else {
+
+//     alert(compra);
+
+// }
+
+                  //exercicio I-4
+
+// let email1 = prompt("Crie um email");
+// let senha1 = prompt("Crie um senha");
+// let email = prompt("Email para entrar")
+// let senha = prompt("Senha para entrar")
+
+// if (email == email1 && senha == senha1) {
+
+//     alert("Login bem sucedido")
     
-    // if (numero > 0){
+// }else{
 
+//     alert("Confirme se seus dados estão corretos")
+// }
 
-    //     alert("Positivo");  
-        
-    // } else {
+               //avaçado 1
 
-
-    // if (numero == 0){
-
-    //     alert ("O numero e zero");}
-
-    //     else{
-    //         alert("negativo")
-    //     }
-    // }
-
-    //exercicio 5
-
-    // let idade = prompt("Qual sua idade?")
-
-    // if (idade <= 12) {
-
-    //     alert("Kids")
-        
-    // }
-
-    // else { 
-    //          if (idade >= 18) {
-
-    //     alert("Adulto") }
-
-    // else{
-    
-    //     alert("Adolecente");}
-    // }
-
-      //exercicio 6
-
-    // let numero = prompt("Insira um numero");
-
-    // if (numero % 2 == 0  ) {
-
-    //     alert("Par")
-        
-    // } else 
-
-    //     alert("Impar")
-    
-    
-            //   INTERMEDIARIO
-
-            //   Exercicio 1
-
-            let numero1 = Number(prompt("Escolha um numero"));
-
-            let numero = Number(prompt("Escolha outro numero"));
-
-            let operacao = Number(prompt("Insira a operação: +, -, *,/"));
-
-            if ( operacao == "+"){
-
-                alert(numero + numero1);
-   
-            } else (operacao == -) {
-
-                alert(numero - numero1 );
-                
-                if(operacao == *) {
-
-                    alert(numero * numero1);
-                    
-                }
-
-                else(operacao == /){
-
-                    alert(numero / numero1 )
-                } 
-            }
-
-             
-
-
-            
-
-            
-        
+               
 
 
 
-        
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
