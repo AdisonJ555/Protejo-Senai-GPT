c2bf30372812963fcae770d45cd5cacc35125806
AdisonJ555 <kaneki.adison@gmail.com>
2025-08-28 21:52:26 -0300

    intermediario

--- javascript/repeticoes.js
+++ javascript/repeticoes.js
@@ -33,15 +33,68 @@
 //     console.log(numero * i)
 
 // }
-let n = Number(prompt("Escolha um numero"));
-let i = ("1");
-while (i <= n - 1) {
+// let n = Number(prompt("Escolha um numero"));
+// let i = 1;
+// let soma = 0;
 
-    console.log(i - n);
+// while (i <= n ){
 
-    i++
+//       soma = soma + i;
+//       console.log(soma)
+//       i++;
+
+// }
+
+//       console.log(soma);
+
+//intermediario -1
+
+// let n =50
+// let i = 0;
+
+
+// while (i <= n ){
+
+
+//       console.log(i)
+//       i = i + 2;
+
+// }
+
+//exercicio -2
+// let max = 100;
+// let min = 1;
+
+
+// Math.floor(Math.random() * (max - min + 1)) + min;
+
+// let aleatorio = Math.floor(Math.random() * (max - min + 1)) + min;
+
+// console.log(aleatorio);
+
+                //   exercicio -2
+
+
+let max = 10 ;
+let min = 1 ;
+
+Math.floor(Math.random() * (max - min + 1)) + min;
+
+let aleatorio = Math.floor(Math.random() * (10 - 1 + 1)) + min;
+
+let numero = "";
+
+do{
     
-}
- alert("Tente novamente";)
-        
+  numero = Number(prompt("Escolha um numero de 1 a 10"));
+  if ( numero = aleatorio){
+    console.log("Parabens");
+
+  } else {
+
+    
+  }
+
+} while(numero != aleatorio)
+
 
